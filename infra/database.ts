7f82a4f8975f729bb0b4cd72d213b8e732a30e65
LucasSousaAl <lucas.sousa277@hotmail.com>
2026-04-13 10:14:27 -0300

    hot fix: port on database

--- infra/database.ts
+++ infra/database.ts
@@ -3,7 +3,7 @@ import { Client, QueryConfig, QueryResultRow } from "pg";
 export async function query<T extends QueryResultRow = any>(queryObject: string | QueryConfig): Promise<T[]> {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT ? Number(process.env.POSTGRES_PORT) : 5432,
+    port: Number(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
